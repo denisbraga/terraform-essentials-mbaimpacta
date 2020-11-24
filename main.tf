9e453803cbc7cd2e5e13e23d94e1f2bb696ba240
denisbraga <drbraga@gmail.com>
2020-11-23 22:34:22 -0300

    Commit Inicial

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("/vagrant/denis-mbaimpacta-turma04-642f904fcd8b.json")
+
+  project     = "denis-mbaimpacta-turma04"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+  }
+  
+# Cria uma VM no Google Cloud
+resource "google_compute_instance" "firstvm" {
+  name         = "website"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  tags         = ["website", "impacta"]
+  
+  # Define a Imagem da VM
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20201014"
+    }
+  }
+
+  # Habilita rede para a VM com um IP público
+  network_interface {
+    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
+
+    access_config {
+      // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
+    }
+  }
+}
+
+# Retorna o IP da VM criada
+output "ip" {
+  value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
+}
