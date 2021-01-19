1585b9cbdc6c17c01b116bf13c31012d8bd05ec3
denisbraga <71675415+denisbraga@users.noreply.github.com>
2021-01-18 22:17:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "google" {
   version = "3.5.0"
 
 
-  project     = "denis-mbaimpacta-turma04"
+  project     = "denis1-mbaimpacta-turma04"
   region  = "us-central1"
   zone    = "us-central1-c"
   }
